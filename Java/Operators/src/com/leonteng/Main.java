ae0b8587b40eadb26906c6b683a9642b7b0f38da
Leon <li.teng@egnition-hamburg.de>
2018-01-12 23:16:01 +0100

    Operators

---
+++ Java/Operators/src/com/leonteng/Main.java
@@ -0,0 +1,15 @@
+package com.leonteng;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// So operators is quite easy, this one I never learnt about is '?' ternary operator
+        //need to learn more about this.
+        boolean isCar = false;
+        boolean wasCar = isCar ? true : false;
+
+        if (wasCar)
+            System.out.println("wasCar is true");
+        
+    }
+}
